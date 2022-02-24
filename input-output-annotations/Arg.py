e9bd5db008e86a46c6395cef025feb5518f72344
Felix Stutz <fstutz@mpi-sws.org>
2022-02-24 12:01:51 -0500

    Fix Arg class and add function for transformers

--- input-output-annotations/Arg.py
+++ input-output-annotations/Arg.py
@@ -8,6 +8,7 @@ class Arg:
     data : depending on kind
     """
     def __init__(self, kind, data):
+        self.kind = kind,
         match kind:
             case ArgKindEnum.FLAG:
                 self.flag_name = data
@@ -15,5 +16,16 @@ class Arg:
                 self.option_name, self.option_arg = data
             case ArgKindEnum.OPERAND:
                 self.operand_name = data
+            case _:
+                raise Exception("Arg got unknown kind!")
+
+    def get_name_suffix_for_transformer(self):
+        match self.kind:
+            case ArgKindEnum.FLAG:
+                str(self.flag_name)
+            case ArgKindEnum.OPTION:
+                str(self.option_name)
+            case ArgKindEnum.OPERAND:
+                "operand" # flags and options all start with - so no clash of names
             case _:
                 raise Exception("Arg got unknown kind!")
