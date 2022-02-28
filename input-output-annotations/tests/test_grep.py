a1a949dc245406566fbc14dc6bc9e4d2fc19ef73
Felix Stutz <fstutz@mpi-sws.org>
2022-02-28 14:45:56 -0500

    Fix grep when no file to parse is given

--- input-output-annotations/tests/test_grep.py
+++ input-output-annotations/tests/test_grep.py
@@ -42,8 +42,7 @@ def test_grep_3():
 def test_grep_4():
     args = [make_arg_simple(["-f", "dict.txt"]), make_arg_simple(["-e", "*"]), make_arg_simple(["-f", "dict2.txt"])]
     operands = [Operand("in1.txt"),
-                Operand("in2.txt"),
-                Operand("dict.txt")]
+                Operand("in2.txt")]
 
     meta = AnnotationGeneration.get_meta_from_cmd_invocation(cmd_name, args, operands)
 
@@ -53,11 +52,23 @@ def test_grep_4():
 
 def test_grep_5():
     args = [make_arg_simple(["-q"]), make_arg_simple(["-s"])]
-    operands = [Operand("in1.txt"),
-                Operand("in2.txt"),
-                Operand("dict.txt")]
+    operands = [Operand("*"),
+                Operand("in1.txt"),
+                Operand("in2.txt")]
 
     meta = AnnotationGeneration.get_meta_from_cmd_invocation(cmd_name, args, operands)
 
     assert len(meta.get_input_list()) == 2
     assert len(meta.get_output_list()) == 0
+
+
+def test_grep_6():
+    args = []
+    operands = [Operand("*")]
+
+    meta = AnnotationGeneration.get_meta_from_cmd_invocation(cmd_name, args, operands)
+
+    assert len(meta.get_input_list()) == 1  # only stdin
+    assert len(meta.get_output_list()) == 2
+
+# TODO: add each one test case where only stdout and stderr is used (similar to 5)
