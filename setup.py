dbc0c170c6badf1a7734be1cd9694ef056a1ab74
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-06-30 17:13:20 -0400

    Fix setup to include json files
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
-from distutils.core import setup
+# from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
 
 ##
 ## TODO: Rename the package names (and the name of the whole library)
 ##
 setup(name='annotations',
       version='0.1',
-      #   py_modules=['foo'],
+      py_modules=['util_flag_option',
+                  'util_new',
+                  'util_standard'],
       packages=['annotation_generation_new',
+                'annotation_generation_new.annotation_generators',
+                'annotation_generation_new.datatypes',
+                'annotation_generation_new.datatypes.parallelizability',
+                'command_flag_option_info',
+                'config_new',
                 'datatypes_new',
-                'parser_new']
+                'parser_new'],
+    ## TODO: Maybe we can use this?
+    #   packages=find_packages(),
+    #   package_data={'': ['command_flag_option_info/data/*.json']},
+    #   data_files=[('', ['command_flag_option_info/data/*.json'])],
+      include_package_data=True
       )
