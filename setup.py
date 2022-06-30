6178544a740667b968f49c2ea15c9a7e36269479
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-06-30 15:26:30 -0400

    Add a setup.py file
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+##
+## TODO: Rename the package names (and the name of the whole library)
+##
+setup(name='annotations',
+      version='0.1',
+      #   py_modules=['foo'],
+      packages=['annotation_generation_new',
+                'datatypes_new',
+                'parser_new']
+      )
