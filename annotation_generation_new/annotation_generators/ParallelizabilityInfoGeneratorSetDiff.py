88913ce1318a006c70de178a84ee25c0d9cc119b
Felix Stutz <fstutz@mpi-sws.org>
2022-07-20 10:38:48 -0400

    Parallelize spell-grep as done in `future`, i.e., not RR but CC for `set_diff`
    Signed-off-by: Felix Stutz <fstutz@mpi-sws.org>

--- annotation_generation_new/annotation_generators/ParallelizabilityInfoGeneratorSetDiff.py
+++ annotation_generation_new/annotation_generators/ParallelizabilityInfoGeneratorSetDiff.py
@@ -3,4 +3,6 @@ from annotation_generation_new.annotation_generators.ParallelizabilityInfoGenera
 class ParallelizabilityInfoGeneratorSetDiff(ParallelizabilityInfoGeneratorInterface):
 
     def generate_info(self) -> None:
-        self.set_parallelizability_info_for_stateless()
+        self.append_to_parallelizer_list_cc_seq_conc()
+        # issues with RR (also in future due to tee I think)
+        # self.set_parallelizability_info_for_stateless()
