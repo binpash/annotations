187d81bea5777c9a96d85f8c7d7eb82412dd8196
Felix Stutz <fstutz@mpi-sws.org>
2022-07-20 11:06:34 -0400

    Fix annotation for `tee`

--- annotation_generation_new/annotation_generators/InputOutputInfoGeneratorTee.py
+++ annotation_generation_new/annotation_generators/InputOutputInfoGeneratorTee.py
@@ -4,5 +4,7 @@ from annotation_generation_new.annotation_generators.InputOutputInfoGenerator_In
 class InputOutputInfoGeneratorTee(InputOutputInfoGeneratorInterface):
 
     def generate_info(self) -> None:
-        self.all_but_first_operand_is_streaming_output()
+        self.set_implicit_use_of_stdin()
+        # copies to both operands and stdout
         self.set_implicit_use_of_stdout()
+        self.all_operands_are_streaming_outputs()
