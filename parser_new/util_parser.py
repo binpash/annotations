c876155176b55687d353ae3bb8bfb9aaac90f9ea
Felix Stutz <fstutz@mpi-sws.org>
2022-07-25 16:34:09 -0400

    Refactor to have defaults for json-files

--- parser_new/util_parser.py
+++ parser_new/util_parser.py
@@ -10,6 +10,11 @@ def get_json_data(cmd_name):
         with open(command_json_fn_absolute) as f:
             json_data = json.load(f)
     except FileNotFoundError:
-        raise Exception(f'File {command_json_fn_absolute} not found.')
+        try:
+            command_json_fn_absolute: str = os.path.join(ROOT_DIR, 'command_flag_option_info/data/_default_data_for_commands.json')
+            with open(command_json_fn_absolute) as f:
+                json_data = json.load(f)
+        except FileNotFoundError:
+            raise Exception(f'json-File for default values not found.')
     return json_data
 
