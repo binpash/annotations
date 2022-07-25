470a7a457fdaa92c2d28658485ee490dd574fa9c
Felix Stutz <fstutz@mpi-sws.org>
2022-07-25 16:14:59 -0400

    Refactor to have defaults for IO-Info and ParallelizabilityInfo; remove annotation generators accordingly

--- annotation_generation_new/annotation_generators/ParallelizabilityInfoGeneratorDiff.py
+++
@@ -1,7 +0,0 @@
-from annotation_generation_new.annotation_generators.ParallelizabilityInfoGenerator_Interface import ParallelizabilityInfoGeneratorInterface
-
-class ParallelizabilityInfoGeneratorDiff(ParallelizabilityInfoGeneratorInterface):
-
-    def generate_info(self) -> None:
-        # there might be space for optimizations
-        pass
--- annotation_generation_new/annotation_generators/ParallelizabilityInfoGeneratorRm.py
+++
@@ -1,6 +0,0 @@
-from annotation_generation_new.annotation_generators.ParallelizabilityInfoGenerator_Interface import ParallelizabilityInfoGeneratorInterface
-
-class ParallelizabilityInfoGeneratorRm(ParallelizabilityInfoGeneratorInterface):
-
-    def generate_info(self) -> None:
-        pass
