25fb92cea432bec4c3d521a3a74517c0768aa34a
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-09-26 17:09:27 -0400

    add python requirement and change version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 ## TODO: Rename the package names (and the name of the whole library)
 ##
 setup(name='pash-annotations',
-      version='0.1',
+      version='0.1.1',
       py_modules=['util_flag_option',
                   'util_new',
                   'util_standard'],
@@ -23,6 +23,7 @@ setup(name='pash-annotations',
       ## Necessary for the markdown to be properly rendered
       long_description=long_description,
       long_description_content_type="text/markdown",
+      python_requires='>=3.8',
     ## TODO: Maybe we can use this?
     #   packages=find_packages(),
     #   package_data={'': ['command_flag_option_info/data/*.json']},
