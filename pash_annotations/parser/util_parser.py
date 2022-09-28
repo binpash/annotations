ccfe6b65b04094baff4c21888a14555da9fece1f
Felix Stutz <fstutz@mpi-sws.org>
2022-09-28 10:53:30 +0200

    Another attempt to fix retrieval of command_flag_option_info data

--- pash_annotations/parser/util_parser.py
+++ pash_annotations/parser/util_parser.py
@@ -1,27 +1,20 @@
 import json
-# import os
-import pkgutil
-
-# from pash_annotations.config.definitions import ROOT_DIR
+import os
 
 def get_json_data(cmd_name):
     command_json_fn = f'{cmd_name}.json'
     # get man page data for command as dict
-    # command_json_fn_absolute: str = os.path.join(ROOT_DIR, 'pash_annotations/command_flag_option_info/data', command_json_fn)
-    command_json_fn_absolute: str = "command_flag_option_info/data" + command_json_fn
+    command_json_fn_absolute: str = os.path.join(os.path.dirname(__file__), '../command_flag_option_info/data', command_json_fn)
     try:
-    #     with open(command_json_fn_absolute) as f:
-    #         json_data = json.load(f)
-        pkgutil.get_data(__name__, command_json_fn_absolute)
-    # except FileNotFoundError:
-    except Exception:
+        with open(command_json_fn_absolute) as f:
+            json_data = json.load(f)
+    except FileNotFoundError:
         try:
-    #         command_json_fn_absolute: str = os.path.join(ROOT_DIR, 'pash_annotations/command_flag_option_info/data/_default_data_for_commands.json')
-            command_json_fn_absolute: str = "command_flag_option_info/data/_default_data_for_commands.json"
+            command_json_fn_absolute: str = os.path.join(os.path.dirname(__file__), '../command_flag_option_info/data',
+                                                         '_default_data_for_commands.json')
             with open(command_json_fn_absolute) as f:
                 json_data = json.load(f)
-        # except FileNotFoundError:
-        except Exception:
+        except FileNotFoundError:
             raise Exception(f'json-File for default values not found.')
     return json_data
 
