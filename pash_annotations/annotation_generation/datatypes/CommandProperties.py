6618d1831254eaca0995ae87afb81076dc9cee0f
Felix Stutz <fstutz@mpi-sws.org>
2022-09-28 09:50:35 +0200

    Try to get data of command_flag_info into package and minor

--- pash_annotations/annotation_generation/datatypes/CommandProperties.py
+++ pash_annotations/annotation_generation/datatypes/CommandProperties.py
@@ -1,4 +1,4 @@
-from util_standard import standard_repr
+from pash_annotations.util_standard import standard_repr
 from pash_annotations.util import return_default_if_none_else_itself
 from typing import Dict, Any, Optional
 
