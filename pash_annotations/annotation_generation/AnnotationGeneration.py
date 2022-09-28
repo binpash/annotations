bcd5f8acd521988838f1b8611a185535b84bb4e1
Felix Stutz <fstutz@mpi-sws.org>
2022-09-28 11:34:08 +0200

    clean up

--- pash_annotations/annotation_generation/AnnotationGeneration.py
+++ pash_annotations/annotation_generation/AnnotationGeneration.py
@@ -60,7 +60,6 @@ for FILENAME_MODULE_PAIR in inputoutput_info_generator_file_module_names + \
     try:
         exec(import_str)
     except ModuleNotFoundError:
-        print("not imported: " + import_str)
         pass # it's fine if some do not exist, we catch that later
 
 
@@ -76,7 +75,6 @@ def get_input_output_info_from_cmd_invocation(cmd_invocation : CommandInvocation
         info_generator_object.generate_info()
         return info_generator_object.get_info()
     except Exception as sth: # module does not exist
-        print("Exc: " + str(sth))
         return None
 
 def get_parallelizability_info_from_cmd_invocation(cmd_invocation : CommandInvocationInitial) -> Optional[ParallelizabilityInfo]:
