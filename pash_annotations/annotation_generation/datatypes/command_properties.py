a4492e08ca86157a52b41289836d15f5f9788089
Forthoney <castlehoneyjung@gmail.com>
2023-11-25 12:15:13 -0500

    rename files where applicable to snake_case

--- pash_annotations/annotation_generation/datatypes/command_properties.py
+++ pash_annotations/annotation_generation/datatypes/command_properties.py
@@ -1,12 +1,14 @@
-from pash_annotations.util_standard import standard_repr
-from pash_annotations.util import return_default_if_none_else_itself
+from util_standard import standard_repr
+from util import return_default_if_none_else_itself
 from typing import Dict, Any, Optional
+
 
 # TODO: use this in construction of Info (under the hood with setter-functions)
 class CommandProperties:
-
     def __init__(self, dict_prop_name_value: Optional[Dict[str, Any]] = None) -> None:
-        self.dict_prop_name_value: Dict[str, Any] = return_default_if_none_else_itself(dict_prop_name_value, dict())
+        self.dict_prop_name_value: Dict[str, Any] = return_default_if_none_else_itself(
+            dict_prop_name_value, dict()
+        )
 
     def __repr__(self) -> str:
         return standard_repr(self)
@@ -16,3 +18,4 @@ class CommandProperties:
 
     def get_property_value(self, prop_name: str):
         return self.dict_prop_name_value.get(prop_name, None)
+
